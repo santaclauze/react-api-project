17a257adefb1b2af01224ab4de2767365da1b0af
Phill <phillip-shields@live.nmit.ac.nz>
2019-11-18 12:07:50 +1300

    console log success

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useFetchPosition } from './FetchPosition'
+
+const Display = () => {
+ 
+  // use your own hook to load the data you need
+  const { location, loading, error } = useFetchPosition()
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>{error}</div>
+
+  return (
+    
+      
+        <h1 className="bg-gray-900 text-3xl">{location.message}</h1>
+     
+  )
+}
+export default Display
---
+++ src/components/FetchPosition.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+export const issUrl = "http://api.open-notify.org/iss-now.json";
+
+export const useFetchPosition = () => {
+
+  // First we define the necessary states for our hook
+  // this includes book, the loading state and potential errors
+  const [location, setLocation] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+ 
+  useEffect(() => {
+
+    // First we set the loading and error states
+    setLoading(true)
+    setError(null)
+
+    fetch(issUrl)
+      .then(res => res.json())
+      .then(json => {
+        setLoading(false)
+        if (json.message) {
+          setLocation(json.message)
+          console.log(json.message)
+        } else {
+          setLocation([])
+        }
+      })
+      .catch(err => {
+        setError(err)
+        setLoading(false)
+      })
+  }, [])
+  return { location, loading, error }
+}
+
